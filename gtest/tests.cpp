e5966144b0b9404af748cf0e0193decf52907bcc
Vavik16 <114207567+Vavik16@users.noreply.github.com>
2023-03-23 15:44:17 +0300

    Update tests.cpp

--- gtest/tests.cpp
+++ gtest/tests.cpp
@@ -18,8 +18,8 @@ ASSERT_THROW(acc.ChangeBalance(120), std::runtime_error);
 }
 
 TEST(Banking, Transaction) {
-Account accA(1, 2500);
-Account accB(2, 2500);
+Account accA(1, 1500);
+Account accB(2, 1500);
 Transaction transaction;
 
 ASSERT_EQ(transaction.fee(), 1);
@@ -31,9 +31,9 @@ ASSERT_THROW(transaction.Make(accB, accB, 1000), std::logic_error);
 ASSERT_THROW(transaction.Make (accA, accB, 40), std::logic_error); 
 transaction.set_fee(100); 
 ASSERT_EQ(transaction.Make(accA, accB, 150), false); 
-ASSERT_EQ(transaction.Make(accA, accB, 3000), false); 
+ASSERT_EQ(transaction.Make(accA, accB, 2000), false); 
 
 ASSERT_EQ(transaction.Make(accA, accB, 1200), true); 
-ASSERT_EQ(accB.GetBalance(), 3700);
-ASSERT_EQ(accA.GetBalance(), 1200); 
+ASSERT_EQ(accB.GetBalance(), 2700);
+ASSERT_EQ(accA.GetBalance(), 300); 
 }
